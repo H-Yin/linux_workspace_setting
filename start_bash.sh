bc63e8d2be0beff2406392fdb962635ea9d62530
H.Yin <csustyinhao@gmail.com>
2018-11-02 11:02:52 +0000

    add a new script

---
+++ start_bash.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+##################################################################
+#  File        : start_bash.sh
+#  Author      : H.Yin
+#  Email       : csustyinhao@gmail.com
+#  Created     : 2018-11-02 11:01:42(+0000)
+#  Modified    : 2018-11-02 11:01:45(+0000)
+#  GitHub      : https://github.com/H-Yin/
+#  Description : 
+#################################################################
+
+

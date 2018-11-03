0d1e0454197fab131c2cb47e2d0657b7e2dc9d5c
H.Yin <csustyinhao@gmail.com>
2018-11-03 18:04:15 +0800

    add git-completion

--- start_git.sh
+++ start_git.sh
@@ -5,7 +5,7 @@
 #  Author      : H.Yin
 #  Email       : csustyinhao@gmail.com
 #  Created     : 2018-11-02 08:52:11(+0000)
-#  Modified    : 2018-11-02 09:10:54(+0000)
+#  Modified    : 2018-11-03 18:02:39(+0800)
 #  GitHub      : https://github.com/H-Yin/linux_workspace_setting
 #  Description : install and Config GIT
 #################################################################
@@ -25,13 +25,24 @@ else
     fi
 fi
 # install dependences
-
+echo "Step-1 : Check and install git ..."
 git --version >/dev/null 2>&1
 if [[ $? -ne 0 ]]; then
     sudo $RMP install -y git >/dev/null 2>&1
     if [[ $? -ne 0 ]]; then echo "ERROR : Unable to install Git."; exit 127; fi
 fi
 
+if [[ -f /etc/bash_completion.d/git ]]; then
+    echo "Add git-completion ..."
+cat >> ~/.bashrc <<-EOF
+if [[ -f /etc/bash_completion.d/git ]]; then
+    . /etc/bash_completion.d/git
+fi
+EOF
+    source ~/.bashrc
+fi
+
+echo "Step-2 : Config git options..."
 git config --global user.name "H.Yin"
 git config --global user.email "csustyinhao@gmail.com"
 git config --global color.advice always
@@ -40,4 +51,6 @@ git config --global color.status always
 git config --global color.branch always
 git config --global color.showBranch always
 
+echo ""
 git config -l
+

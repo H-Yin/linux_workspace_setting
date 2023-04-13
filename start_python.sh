dc406ec2cb7db0d6856a7d5e3678e664071ddb6c
HaoYin <csustyinhao@gmail.com>
2023-04-13 16:39:16 +0800

    update start_python

--- start_python.sh
+++ start_python.sh
@@ -5,7 +5,7 @@
 #  Author      : H.Yin
 #  Email       : csustyinhao@gmail.com
 #  Created     : 2018-11-04 10:16:21(+0000)
-#  Modified    : 2019-08-01 14:09:00(+0800)
+#  Modified    : 2023-04-13 15:29:35(+0800)
 #  GitHub      : https://github.com/H-Yin/linux_workspace_setting
 #  Description : install Anaconda and Jupyter
 #################################################################
@@ -16,46 +16,54 @@ BASEDIR=$(cd `dirname $0`; pwd)
 
 ANACONDA_FILE=`echo $ANACONDA_URL | awk -F/ '{print $NF}'`
 
-echo "Step-1: Download Anaconda installer ..."
-if [[ ! -f $ANACONDA_FILE ]]; then
-    wget -nc $ANACONDA_URL
-fi
-exitcode=$?
-if [[ $exitcode -ne 0 ]]; then
-    echo "ERROR : download Anaconda installer failed."
-    exit $exitcode
-fi
-
-echo "Step-2: Verify the data integrity of the Anaconda installer ..."
-LOCAL_SUM=`md5sum $ANACONDA_FILE | awk '{print $1}'`
-if [[ "$ANACONDA_MD5" != "$LOCAL_SUM" ]]; then
-    echo "ERROR : Anaconda installer file was broken."
-    exit 127
-fi
-
-echo "Step-3: Install Anaconda ..."
-rm -rf $HOME/anaconda3
-bash $ANACONDA_FILE -b -p $HOME/anaconda3 -f
-exitcode=$?
-if [[ $exitcode -ne  0 ]]; then
-    echo "ERROR : install anaconda dailed."
-    exit $exitcode
-fi
-
-echo "Step-4: Add Anaconda to PATH ..."
-cat >> $HOME/.bashrc <<"EOF"
-export PATH="$HOME/anaconda3/bin:$PATH"
-EOF
-source $HOME/.bashrc
 
 conda --version >/dev/null 2>&1
 exitcode=$?
 if [[ $exitcode -ne 0 ]];then
-    echo "ERROR : Anaconda3 can't run properly."
-    exit $exitcode
+    echo "Step-1: Download Anaconda installer ..."
+    if [[ ! -f $ANACONDA_FILE ]]; then
+        wget -nc $ANACONDA_URL
+    fi
+    exitcode=$?
+    if [[ $exitcode -ne 0 ]]; then
+        echo "ERROR : download Anaconda installer failed."
+        exit $exitcode
+    fi
+
+    echo "Step-2: Verify the data integrity of the Anaconda installer ..."
+    LOCAL_SUM=`md5sum $ANACONDA_FILE | awk '{print $1}'`
+    if [[ "$ANACONDA_MD5" != "$LOCAL_SUM" ]]; then
+        echo "ERROR : Anaconda installer file was broken."
+        exit 127
+    fi
+
+    echo "Step-3: Install Anaconda ..."
+    rm -rf $HOME/anaconda3
+    bash $ANACONDA_FILE -b -p $HOME/anaconda3 -f
+    exitcode=$?
+    if [[ $exitcode -ne  0 ]]; then
+        echo "ERROR : install anaconda dailed."
+        exit $exitcode
+    fi
+
+    echo "Step-4: Add Anaconda to PATH ..."
+    echo 'export PATH="$HOME/anaconda3/bin:$PATH"'  >> $HOME/.bashrc
+    source $HOME/.bashrc
+
+    conda --version >/dev/null 2>&1
+    exitcode=$?
+    if [[ $exitcode -ne 0 ]];then
+        echo "ERROR : Anaconda3 can't run properly."
+        exit $exitcode
+    else
+        echo "Install Anaconda3 successfully. "
+    fi
 else
-    echo "Install Anaconda3 successfully. "
+    echo "Anaconda3 has been installed. "
 fi
+
+
+
 
 #echo "Step-5: Config and run jupyter ..."
 #jupyter notebook --generate-config
@@ -65,3 +73,10 @@ fi
 
 #mkdir ~/jupyter
 #nohup jupyter notebook >~/jupyter/jupyter.log 2>&1 &
+
+echo "Step-6: Config python's enviorment"
+cp -f python/tab.py $HOME/.tab.py
+echo 'export PYTHONSTARTUP="$HOME/.tab.py"' >> $HOME/.bashrc
+source $HOME/.bashrc
+
+

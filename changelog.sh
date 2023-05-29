f49d87c11ae5d35ed49943a659b832391a062fbf
HaoYin <csustyinhao@gmail.com>
2023-05-29 23:47:32 +0800

    add change_log script

---
+++ changelog.sh
@@ -0,0 +1,15 @@
+git filter-branch -f --env-filter '
+OLD_EMAIL="yinhao@inagora.cn"
+CORRECT_NAME="HaoYin"
+CORRECT_EMAIL="csustyinhao@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags

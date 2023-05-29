1a32e2c5fe694ed29fe62ca32b968d927ad836de
HaoYin <csustyinhao@gmail.com>
2023-05-29 23:54:58 +0800

    add change_log script

--- changelog.sh
+++ changelog.sh
@@ -4,12 +4,19 @@ CORRECT_NAME="HaoYin"
 CORRECT_EMAIL="csustyinhao@gmail.com"
 if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
 then
-    export GIT_COMMITTER_NAME="$CORRECT_NAME"
     export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
 fi
 if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
 then
-    export GIT_AUTHOR_NAME="$CORRECT_NAME"
     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+
+if [ "$GIT_COMMITTER_NAME" != "$CORRECT_NAME" ]
+then
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_NAME" = "$CORRECT_NAME" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
 fi
 ' --tag-name-filter cat -- --branches --tags
